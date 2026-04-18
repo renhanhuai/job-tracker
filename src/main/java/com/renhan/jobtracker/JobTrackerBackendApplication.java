28c7dbef6cdf968df777188f7366355f48597374
Henry <henry@Henrys-MacBook-Air.local>
2026-04-18 16:46:29 -0400

    job tracker initial set up

---
+++ src/main/java/com/renhan/jobtracker/JobTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.renhan.jobtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renhan/jobtracker/JobTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renhan.jobtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
